d3c8ad853fe8f9236eb3a32251c7a6dd3974dbbf
Chris Rimmer <c@24.org.uk>
2013-06-05 23:04:11 +0100

    Added first version of charProduct

--- src/Stuff.scala
+++ src/Stuff.scala
@@ -1,4 +1,14 @@
 object Stuff {
+  def charProduct(s: String) : Long = {
+    if (s.isEmpty) {
+      0
+    } else {
+      var product = 1L
+      for (c <- s) product = product * c
+      product
+    }
+  }
+
   def signum (x: Int) = {if (x < 0) -1 else if (x > 0) 1 else 0}
 
   def countdown (n: Int) {for (i <- (0 to n).reverse) println(i)}
--- test/StuffTest.scala
+++ test/StuffTest.scala
@@ -3,16 +3,32 @@ import Stuff._
 
 class StuffTest extends FunSuite {
 
-  test("positive number gives 1") {
-    assert(signum(10) == 1)
+  test("Positive number gives 1") {
+    assert(signum(10) === 1)
   }
 
-  test("negative number gives -1") {
-    assert(signum(-9) == -1)
+  test("Negative number gives -1") {
+    assert(signum(-9) === -1)
   }
 
-  test("zero gives zero") {
-    assert(signum(0) == 0)
+  test("Zero gives zero") {
+    assert(signum(0) === 0)
+  }
+
+  test("Empty string gives zero") {
+    assert(charProduct("") === 0)
+  }
+
+  test("A gives 65") {
+    assert(charProduct("A") === 65)
+  }
+
+  test("AB gives 65*66") {
+    assert(charProduct("AB") === 65*66)
+  }
+
+  test("Hello gives 9415087488") {
+    assert(charProduct("Hello") === 9415087488L)
   }
 
 }
