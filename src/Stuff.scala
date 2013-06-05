f910094e0ca9598a116cab37120427534faae315
Chris Rimmer <c@24.org.uk>
2013-06-05 23:22:19 +0100

    Added recursive version

--- src/Stuff.scala
+++ src/Stuff.scala
@@ -1,7 +1,8 @@
 object Stuff {
 
   def charProduct(s: String) : Long = {
-    s.foldLeft(1L)((a, b) => a*b)
+    // s.foldLeft(1L)((a, b) => a*b)
+    if (s.isEmpty) 1 else s.head * charProduct(s.tail)
   }
 
   def signum (x: Int) = {if (x < 0) -1 else if (x > 0) 1 else 0}
